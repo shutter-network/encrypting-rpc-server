f575eae5dda7130c68f31331ab396e8926f7ddec
blockchainluffy <jainpunit1997@gmail.com>
2024-08-29 14:25:57 +0530

    feat: added migration file and changed compose file

---
+++ docker/migration.sql
@@ -0,0 +1,15 @@
+CREATE TABLE transaction_details (
+    address VARCHAR(255) NOT NULL,
+    nonce BIGINT NOT NULL,
+    tx_hash VARCHAR(255) NOT NULL,
+    encrypted_tx_hash VARCHAR(255) NOT NULL,
+    submission_time BIGINT NOT NULL,
+    inclusion_time BIGINT NOT NULL,
+    is_cancellation BOOLEAN NOT NULL,
+    PRIMARY KEY (address, nonce, tx_hash)
+);
+CREATE INDEX idx_address_nonce on transaction_details (address, nonce);
+CREATE INDEX idx_tx_hash on transaction_details (tx_hash);
+
+
+CREATE PUBLICATION mypub FOR TABLE transaction_details;
