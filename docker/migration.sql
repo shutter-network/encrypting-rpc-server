aa47818f397fd7260f2bb9be5c91930773cd0658
blockchainluffy <jainpunit1997@gmail.com>
2024-08-30 15:57:34 +0530

    fix: updated migration

--- docker/migration.sql
+++ docker/migration.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS transaction_details (
     address VARCHAR(255) NOT NULL,
     nonce BIGINT NOT NULL,
     tx_hash VARCHAR(255) NOT NULL,
-    encrypted_tx_hash VARCHAR(255) NOT NULL,
+    encrypted_tx_hash VARCHAR(255),
     submission_time BIGINT NOT NULL,
-    inclusion_time BIGINT NOT NULL,
+    inclusion_time BIGINT,
     is_cancellation BOOLEAN NOT NULL,
-    PRIMARY KEY (address, nonce, tx_hash)
+    PRIMARY KEY (address, nonce, tx_hash, encrypted_tx_hash)
 );
 CREATE INDEX IF NOT EXISTS idx_address_nonce on transaction_details (address, nonce);
 CREATE INDEX IF NOT EXISTS idx_tx_hash on transaction_details (tx_hash);
